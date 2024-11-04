20eff5f370a5187c687d95bdb70bcf76dc736c9e
cerna234 <cerna2424@gmail.com>
2024-11-03 19:57:26 -0800

    Frontend Setup

---
+++ Frontend/defensethreatfrontend/src/Components/SensorStatus.js
@@ -0,0 +1,20 @@
+import "../Components/SensorStatus.css"
+
+function SensorStatusModule() {
+  return (
+    <div className="SensorStatusContainer">
+        <div className="sensorStatus">
+            <h1>STATUS: </h1>
+            <button>BUTTON</button>
+
+        </div>
+        <div>
+            
+        </div>
+        
+      
+    </div>
+  );
+}
+
+export default SensorStatusModule;
---
+++ Frontend/defensethreatfrontend/src/Pages/Home.js
@@ -0,0 +1,14 @@
+
+import SensorStatusModule from "../Components/SensorStatus"
+
+function Home() {
+  return (
+    <div>
+       
+            
+        <SensorStatusModule/>
+    </div>
+  );
+}
+
+export default Home;
