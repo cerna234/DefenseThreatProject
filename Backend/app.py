b80346855b9363d54e800a7621693b0fb45aa59b
cerna234 <cerna2424@gmail.com>
2024-11-05 22:31:28 -0800

    Frontend Updated added ability to update sensor status (Development Testing), Added CORS to API to allow connections from frontend

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify
 from routes.sensor_routes import sensor_bp
-
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 # Register the sensor routes
 app.register_blueprint(sensor_bp)
 
