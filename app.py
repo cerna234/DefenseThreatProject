bd7569597e8b09256a095c050deda475e94d0e9a
cerna234 <cerna2424@gmail.com>
2024-11-12 21:56:15 -0800

    update reqs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from routes.sensor_routes import sensor_bp
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -13,4 +14,5 @@ def home():
     return jsonify({'HOME': 'HOME TEST'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
