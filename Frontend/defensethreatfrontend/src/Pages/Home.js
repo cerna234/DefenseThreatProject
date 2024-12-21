68ae257b846eb45b2743097bda07c037a88c171a
cerna234 <51102686+cerna234@users.noreply.github.com>
2024-12-20 16:14:22 -0800

    Revert "Cleaned Up Backend Branch"

---
+++ Frontend/defensethreatfrontend/src/Pages/Home.js
@@ -0,0 +1,31 @@
+
+import SensorStatusModule from "../Components/SensorStatus"
+import SensorDataModule from "../Components/sensorData"
+import HistoricalEnergyComponent from "../Components/HistoricalEnergyComponent"
+import "../Pages/Home.css"
+
+function Home() {
+  return (
+    <div className="homePageContainer">       
+
+        <div className="homePageOverLay"></div>
+          <div className="gridSection">
+            <SensorDataModule viewAllData="false"/>
+          </div>
+          <div className="DataSectionModule">
+            
+              <SensorDataModule viewAllData="true"/>
+              <div className="dataModules">
+                <HistoricalEnergyComponent/>
+                <SensorStatusModule/>
+                
+              </div>
+            
+          </div>
+     
+       
+    </div>
+  );
+}
+
+export default Home;
