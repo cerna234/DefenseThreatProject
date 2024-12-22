78e894f9f06f3004f1b9fb968b9b9f9437940ae0
cerna234 <cerna2424@gmail.com>
2024-12-22 09:38:22 -0800

    reqs

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/pi/Desktop/DefenseThreatProject/Backend/venv"
+set -gx VIRTUAL_ENV "/home/pi/Desktop/DefenseThreatProject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
