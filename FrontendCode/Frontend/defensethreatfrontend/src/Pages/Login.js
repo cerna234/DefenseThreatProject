cb59c047149fe7203b6bc16dc660c00d546df5ef
cerna234 <cerna2424@gmail.com>
2025-01-08 16:10:45 -0800

    basic validation added for now

--- FrontendCode/Frontend/defensethreatfrontend/src/Pages/Login.js
+++ FrontendCode/Frontend/defensethreatfrontend/src/Pages/Login.js
@@ -5,20 +5,55 @@ function LoginScreen() {
 
 
   /*const [loginData,setLoginData] = useState();*/
-  const [username,setUsername] = useState("TEST")
-  const [password,setPassword] = useState("TEST");
+  const [username,setUsername] = useState()
+  const [password,setPassword] = useState();
+
 
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
 
 
     e.preventDefault(); // Prevent the default form submission behavior
-    console.log(username);
-    console.log(password);
-    navigate('/radar')
-    //handle logic to allow or not allow users into radar
+    
+    const url = "https://defenseproject-fca5305c6d88.herokuapp.com/login"
+    try {
+      
 
+      const payload = {
+        username: username,
+        password: password
+      }
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload),
+      });
+    
+      if(response.status == 200){
+        
+        const data = await response.json();
+  
+       console.log(data)
+  
+         
+        if(data.returnedLoginData !== "Incorrect Username or password" ){
+          navigate('/radar')
+        }
+      }
+     
+    
+    
+
+    } catch (error) {
+        console.error(`Error fetching data from ${url}:`, error);
+    }
+    //handle logic to allow or not allow users into radar
+   
+    
 
     // You can send formData to an API or process it as needed
   };
