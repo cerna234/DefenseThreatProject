8f265ceafb063d8b6a1f134248e61386694a8697
cerna234 <cerna2424@gmail.com>
2025-01-21 06:45:38 -0800

    cookie validation

--- FrontendCode/Frontend/defensethreatfrontend/src/Pages/Radar.js
+++ FrontendCode/Frontend/defensethreatfrontend/src/Pages/Radar.js
@@ -1,10 +1,47 @@
-
+import { useState,useEffect } from "react"
+import { BrowserRouter as Navigate, useNavigate } from 'react-router-dom';
 import SensorStatusModule from "../Components/SensorStatus"
 import SensorDataModule from "../Components/sensorData"
 import HistoricalEnergyComponent from "../Components/HistoricalEnergyComponent"
+import axios from "axios";
 import "../Pages/Radar.css"
 
 function RadarPage() {
+
+  const navigate = useNavigate();
+  const[loginValid,setLoginValid] = useState(true);
+  const apiUrl = `${process.env.REACT_APP_API_URL}/getcookie`;
+
+  const validateLogin = () => {
+
+    axios.get(apiUrl, {
+      
+    })
+      .then(response => {
+        if(response.status === 200){
+          
+         console.log("Response") 
+         console.log(response.data)
+
+         if(response.data !== "test"){
+           navigate("/")
+         }
+        }
+      })
+      .catch(error => {
+        console.log("error", error.message)
+      })
+  }
+
+  
+
+ 
+
+  useEffect( () => {
+    validateLogin();
+  
+  },[])
+
   return (
     <div className="homePageContainer">       
 
