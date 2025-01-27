2e804b8063fea26fcfa7bb009ab5ac901709d916
cerna234 <cerna2424@gmail.com>
2025-01-26 20:43:06 -0800

    auth Token validation

--- FrontendCode/Frontend/defensethreatfrontend/src/Pages/Radar.js
+++ FrontendCode/Frontend/defensethreatfrontend/src/Pages/Radar.js
@@ -1,46 +1,60 @@
-import { useState,useEffect } from "react"
-import { BrowserRouter as Navigate, useNavigate } from 'react-router-dom';
+import { useEffect } from "react"
+import { BrowserRouter as Navigate,useNavigate } from 'react-router-dom';
 import SensorStatusModule from "../Components/SensorStatus"
 import SensorDataModule from "../Components/sensorData"
 import HistoricalEnergyComponent from "../Components/HistoricalEnergyComponent"
-import axios from "axios";
+
 import "../Pages/Radar.css"
 
 function RadarPage() {
 
   const navigate = useNavigate();
-  const[loginValid,setLoginValid] = useState(true);
+
+  //
+  
   const apiUrl = `${process.env.REACT_APP_API_URL}/getcookie`;
 
-  const validateLogin = () => {
-
-    axios.get(apiUrl, {
-      
-    })
-      .then(response => {
-        if(response.status === 200){
-          
-         console.log("Response") 
-         console.log(response.data)
-
-         if(response.data !== "test"){
-           navigate("/")
-         }
-        }
-      })
-      .catch(error => {
-        console.log("error", error.message)
-      })
-  }
-
-  
-
  
+  
+  //needed for eslint error
+  
+  Navigate("/radar")
 
   useEffect( () => {
+ 
+    const validateLogin = () => {
+
+      try{
+  
+  
+    
+          fetch(apiUrl, {
+            method: 'GET',
+            credentials: 'include'  // Ensure cookies are sent with the request
+          })
+            .then(response => response.json())
+            .then(
+              data => {
+                if(data.auth_token){
+                  console.log(data)
+                }
+                else{
+                  navigate("/")
+                }
+              
+              }
+            );
+  
+      } catch(e){
+        console.log(e)
+      }
+      
+    }
+  
+    
     validateLogin();
   
-  },[])
+  },[apiUrl,navigate])
 
   return (
     <div className="homePageContainer">       
